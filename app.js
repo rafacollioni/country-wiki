af2cb6996b83d7d97c3b2657dad60ba1edad0abd
rafacollioni <rafa.collioni@gmail.com>
2022-09-21 20:15:45 -0300

    Finalizado :D

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const searchBtn = document.querySelector('.search-button'),
     searchInput = document.querySelector('.search-input'),
+    countryInfo = document.querySelector('.country-info'),
     countryFlag = document.querySelector('.country-flag'),
-    countryName = document.querySelector('.country-name'),
-    countryCapital = document.querySelector('capital'),
+    countryNameHtml = document.querySelector('.country-name'),
+    countryCapital = document.querySelector('.capital'),
     countryContinent = document.querySelector('.continente'),
     countryPopulation = document.querySelector('.populacao'),
     countryCurrency= document.querySelector('.moeda'),
-    countryLanguage = document.querySelector('.lingua');
+    countryLanguage = document.querySelector('.lingua'),
+    errorMessage = document.querySelector('.error');
 
 
 
@@ -17,6 +19,14 @@ searchBtn.addEventListener('click', () => {
             fetch(finalURL).then((response) => {
                 if (response.status == 200){
                     response.json().then((data) => {
+                    
+                        //    Habilitando a Aba de Dados
+ 
+                        countryInfo.classList.remove("disabled");
+                        errorMessage.classList.add("disabled");
+                
+                        //    Armazenando os Dados Necesários em Variáveis 
+
                         let requestDatA = data[0];
                         let name = data[0].name.common;
                         let population = data[0].population;
@@ -26,9 +36,22 @@ searchBtn.addEventListener('click', () => {
                         currencies = currencies.toString().split(",").join(", ");
                         let languages = Object.values(data[0].languages);
                         let flagImage = Object.values(data[0].flags);
+
+                    //  Fazendo as Alteraçõs na DOM
+
                         countryFlag.setAttribute("src", flagImage[0]);
+                        countryNameHtml.innerHTML = `${name}`
+                        countryCapital.innerHTML = `<strong>Capital:</strong> ${capital}</p>`;
+                        countryContinent.innerHTML = `<strong>Region:</strong> ${region}</p>`;
+                        countryLanguage.innerHTML = `<strong>Languages:</strong> ${languages}</p>`;
+                        countryPopulation.innerHTML = `<strong>Population:</strong> ${population}</p>`
+                        countryCurrency.innerHTML = `<strong>Currency:</strong> ${currencies}</p>`
                     })
-            }});
+            }else {
+                errorMessage.classList.remove("disabled");
+
+            }
+        });
             });
 
 
